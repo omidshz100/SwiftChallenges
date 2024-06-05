539ec3601dbbc8bb93a4c3c0ba05d7e44dde6d34
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-06-05 12:36:55 +0200

    Initial Commit

---
+++ SwiftChallenges/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftChallenges
+//
+//  Created by Omid Shojaeian Zanjani on 05/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftChallenges/SwiftChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallengesApp.swift
+//  SwiftChallenges
+//
+//  Created by Omid Shojaeian Zanjani on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
